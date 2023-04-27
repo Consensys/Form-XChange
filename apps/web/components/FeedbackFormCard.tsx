65b74254ef9a1a1ba990b2d0218ae7dd0dce6848
Alejandro José Mena <96505286+cxalem@users.noreply.github.com>
2023-04-27 10:06:38 -0500

    Fix submit feedback (#21)
    - Now we see the 10 latest created from first
    - Loading state for feedbackFormCard data fetching (Now using SWR)
    - When creating a form, we are now waiting for the transaction and
    pushing to main after confirmation!
    - Console Logs removed
    
    ---------
    
    Co-authored-by: meowy <90390363+meowyx@users.noreply.github.com>

--- apps/web/components/FeedbackFormCard.tsx
+++ apps/web/components/FeedbackFormCard.tsx
@@ -1,13 +1,11 @@
-import { useEffect, useState } from "react";
-import contract from "packages/form-XChange/build/contracts/FeedbackForm.json";
+import { useState } from "react";
 import { truncateEthAddress } from "../utils/networks";
 import FeedbacksModal from "./FeedbacksModal";
 import Button from "./Button";
 import { H3, Text } from "./Text";
 import { useNetwork } from "../hooks/useNetwork";
-import { getFeedbackFormInstanceContract } from "../lib/contract.ts/feedBackFormInstanceContract";
 import useSwr from "swr";
-import { ethers } from "ethers";
+import { FeedbackFormCardSkeleton } from "./FeedbackFormCardSkeleton";
 
 type Props = {
   id: number;
@@ -18,56 +16,40 @@ type Props = {
 export const FeedbackFormCard: React.FC<Props> = ({ id, address }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const [hasSubmitedFeedback, setHasSubmitedFeedback] = useState(false);
   const {
-    state: { wallet, isConnected, wrongNetwork },
+    state: { wallet, isConnected },
     connect,
   } = useNetwork();
 
   const fetcher = () =>
     fetch("/api/form-details", {
-      body: JSON.stringify({ address }),
+      body: JSON.stringify({ address, userAddress: wallet }),
       method: "POST",
     }).then((res) => res.json());
 
-  const { data } = useSwr<{ title: string; description: string }>(
-    `formCard-${address}`,
-    fetcher
-  );
-
-  const canFetchFormInfo = isConnected && !wrongNetwork;
-
-  useEffect(() => {
-    if (wallet) {
-      getHasSubmitedFeedback();
-    }
-  }, [wallet]);
-
-  const getHasSubmitedFeedback = async () => {
-    if (canFetchFormInfo) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const contract = getFeedbackFormInstanceContract({ address, provider });
-      const hasSubmited = await contract.getHasProvidedFeedback(wallet!);
-      console.log({ hasSubmited });
-      setHasSubmitedFeedback(hasSubmited);
-    }
-  };
+  const { data, isLoading } = useSwr<{
+    title: string;
+    description: string;
+    hasProvidedFeedback: boolean;
+  }>(`formCard-${address}`, fetcher, {
+    refreshInterval: 1000,
+  });
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
+  if (isLoading) return <FeedbackFormCardSkeleton />;
+
   return (
     <div className="flex flex-col items-center justify-between w-full max-w-2xl gap-2 p-6 mx-auto border shadow-md bg-white md:gap-0 md:flex-row border-primary-blue rounded-xl">
       <header>
-        <H3>{data ? data.title : "loading..."}</H3>
+        <H3>{data?.title}</H3>
         <Text className="font-thin">
           Address: {truncateEthAddress(address)}
         </Text>
       </header>
-      <Text className="font-thin">
-        {data ? data.description : "loading..."}
-      </Text>
-      {hasSubmitedFeedback ? (
+      <Text className="font-thin">{data?.description}</Text>
+      {data?.hasProvidedFeedback ? (
         <Button
           className="py-2 text-center max-w-[200px]"
           href={`/results?address=${address}`}
